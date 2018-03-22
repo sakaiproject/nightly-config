dd549dca9f11f60c3ba9be3eab2bd2cfe81ecf5f
Charles Severance <csev@umich.edu>
2018-03-22 01:03:50 -0400

    Automatically add TsugiCloud as an App Store

--- 12-mysql.sql
+++ 12-mysql.sql
@@ -14,3 +14,7 @@ DROP TABLE UPDATE_SAM_AUTO_IDS;
 CREATE TEMPORARY TABLE UPDATE_SAM_AUTO_IDS AS (SELECT ID FROM SAM_ASSESSMENTBASE_T WHERE ID NOT IN (SELECT ASSESSMENTID FROM SAM_ASSESSMETADATA_T WHERE label = 'automaticSubmission_isInstructorEditable'));
 INSERT INTO SAM_ASSESSMETADATA_T (ASSESSMENTMETADATAID, ASSESSMENTID, LABEL, ENTRY) SELECT NULL, ID, 'automaticSubmission_isInstructorEditable', 'true' FROM UPDATE_SAM_AUTO_IDS;
 DROP TABLE UPDATE_SAM_AUTO_IDS;
+
+-- Add an automatic link to TsugiCloud Test
+INSERT INTO `lti_tools` (`version`, `SITE_ID`, `title`, `allowtitle`, `fa_icon`, `pagetitle`, `allowpagetitle`, `description`, `status`, `visible`, `resource_handler`, `deployment_id`, `launch`, `allowlaunch`, `consumerkey`, `allowconsumerkey`, `secret`, `allowsecret`, `frameheight`, `toolorder`, `allowframeheight`, `siteinfoconfig`, `sendname`, `sendemailaddr`, `allowoutcomes`, `allowroster`, `allowsettings`, `pl_launch`, `pl_linkselection`, `pl_contenteditor`, `pl_importitem`, `pl_fileitem`, `pl_assessmentselection`, `newpage`, `debug`, `custom`, `settings`, `parameter`, `tool_proxy_binding`, `allowcustom`, `lti13`, `lti13_settings`, `xmlimport`, `splash`, `created_at`, `updated_at`, `sha256`) VALUES
+(NULL, NULL, 'TsugiCloud App Store', 1, NULL, 'TsugiCloud App Store', 0, NULL, 0, 0, NULL, NULL, 'https://test.tsugicloud.org/tsugi/lti/store/', 1, '12345', 0, 'secret', 0, NULL, NULL, 0, 0, 1, 1, 1, 1, 1, NULL, 1, 1, NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, NOW(), NOW(), 0);
