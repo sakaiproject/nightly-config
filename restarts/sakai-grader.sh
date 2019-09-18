36e2c1788f88616ca170ae78711229898150a619
Earle Nietzel <earle@longsight.com>
2019-09-18 10:28:24 -0400

    Grader config updated

--- restarts/sakai-grader.sh
+++ restarts/sakai-grader.sh
@@ -6,51 +6,21 @@
 
 BUILD_ID=bin/startup.sh
 CATALINA_BASE=/var/sakai-grader-mysql
-DBSCRIPT="${WORKSPACE}/master-mysql.sql"
-DBNAME=nightly_sakai_grader
+DBNAME=nightly_grader
 
 cp 19.properties ${CATALINA_BASE}/sakai/sakai.properties
 cd ${CATALINA_BASE}
 bin/stop.sh -force || true
 sleep 30
 
-#Only run this between 0 and 5AM
-typeset -i chour=10#`/bin/date +"%H"`
-typeset -i dow=10#`/bin/date +"%u"`
-# This is %u so 1 (Monday) - 7 (Sunday)
-typeset -i dowclear=1
-typeset -i shour=0
-typeset -i ehour=5
-typeset -i cleardb=${CLEAR_DB:-0}
-
-echo "Current hour is ${chour}. Current day is ${dow}"
-
-if (( (${chour} >= ${shour}) && (${chour} <= ${ehour}) )); then
-    echo "Dow is ${dow} clear on ${dowclear}"
-    if (( ${dow} == ${dowclear} )); then
-        cleardb=1
-    fi
-fi
-
 if (( ${cleardb} == 1 )); then
     echo "Clearing database and assets"
     bin/clean-db.sh
-    #Remove Assets
-	rm -rf /var/sakai-assets/sakai-grader-mysql/*
-
 	mysql -h ${MYSQLDB} -u ${DBNAME} -p${MYSQLGRADERDB} -e "drop database ${DBNAME}"
 	sleep 10
 	mysql -h ${MYSQLDB} -u ${DBNAME} -p${MYSQLGRADERDB} -e "create database ${DBNAME} character set utf8"    
 fi
 
 bin/clean-code.sh
-# rm -rf work/Catalina logs/* webapps components shared/lib/ lib temp/* sakai/archive-unzip/*
 tar xzf /tmp/sakai-grader-mysql.tar.gz
 nohup bin/start.sh
-
-if (( ${cleardb} == 1 )); then
-    if [ -f "${DBSCRIPT}" ]; then
-    	sleep 15m 
-        mysql -f -h ${MYSQLDB} -u ${DBNAME} -p${MYSQLQA19DB} ${DBNAME} -e "source ${DBSCRIPT}"
-    fi
-fi
