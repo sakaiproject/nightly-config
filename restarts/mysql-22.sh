8d5d74fe43fd497be1e4ca4390d925ebb8252e5c
Earle Nietzel <earle@longsight.com>
2021-10-05 18:01:47 -0400

    Add 22 server to nightly config

---
+++ restarts/mysql-22.sh
@@ -0,0 +1,50 @@
+#Most of these variables need to be defined externally (in Jenkins)
+#  These include 
+#  CLEAR_DB - (1/0) 
+#  MYSQLDB - Host name for MySQL
+#  MYSQLQA20DB - Password for this database
+
+BUILD_ID=bin/startup.sh
+CATALINA_BASE=/var/sakai22-mysql
+DBSCRIPT="${WORKSPACE}/22-mysql.sql"
+DBNAME=nightly_qa22
+
+cp 22.properties ${CATALINA_BASE}/sakai/sakai.properties
+cd ${CATALINA_BASE}
+bin/stop.sh -force || true
+sleep 30
+
+#Only run this between 0 and 5AM
+typeset -i chour=10#`/bin/date +"%H"`
+typeset -i dow=10#`/bin/date +"%u"`
+# This is %u so 1 (Monday) - 7 (Sunday)
+typeset -i dowclear=1
+typeset -i shour=0
+typeset -i ehour=5
+typeset -i cleardb=${CLEAR_DB:-0}
+
+echo "Current hour is ${chour}. Current day is ${dow}"
+
+if (( (${chour} >= ${shour}) && (${chour} <= ${ehour}) )); then
+    echo "Dow is ${dow} clear on ${dowclear}"
+    if (( ${dow} == ${dowclear} )); then
+        cleardb=1
+    fi
+fi
+
+if (( ${cleardb} == 1 )); then
+    echo "Clearing database and assets"
+    bin/clean-db.sh
+    #Remove Assets
+fi
+
+bin/clean-code.sh
+tar xzf /tmp/sakai22x.tar.gz
+nohup bin/start.sh
+
+if (( ${cleardb} == 1 )); then
+    if [ -f "${DBSCRIPT}" ]; then
+    	sleep 15m 
+        mysql -f -h ${MYSQLDB} -u ${DBNAME} -p${MYSQLQA20DB} ${DBNAME} -e "source ${DBSCRIPT}"
+    fi
+fi
