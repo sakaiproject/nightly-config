b0ecbaa0f8346cf33ab2efe9b15095516cfef58b
Earle Nietzel <earle@longsight.com>
2022-05-05 11:02:21 -0400

    Updating trunk-mysql script removing jenkins DB variables

--- restarts/mysql-trunk.sh
+++ restarts/mysql-trunk.sh
@@ -1,17 +1,19 @@
-#Most of these variables need to be defined externally (in Jenkins)
-#  These include 
+#!/usr/bin/zsh
+
+# Jenkins variables
 #  CLEAR_DB - (1/0) 
-#  MYSQLDB - Host name for MySQL
-#  MYSQLQATRUNKDB - Password for this database
 
-BUILD_ID=bin/startup.sh
 CATALINA_BASE=/var/trunk-mysql
-DBSCRIPT="${WORKSPACE}/master-mysql.sql"
-DBNAME=nightly_trunk
-
 cp master.properties ${CATALINA_BASE}/sakai/sakai.properties
 cd ${CATALINA_BASE}
-bin/catalina.sh stop -force || true
+source bin/common.sh
+
+DBSCRIPT="${WORKSPACE}/master-mysql.sql"
+DBHOST=$(echo $PROPERTIES["url@javax.sql.BaseDataSource"] | $GREP_CMD -oP '(?<=:\/\/).+(?=:)')
+DBNAME=$(echo $PROPERTIES["url@javax.sql.BaseDataSource"] | $GREP_CMD -oP '(?<=\/)\w+(?=\?)')
+DBUSER=$PROPERTIES["username@javax.sql.BaseDataSource"]
+DBPASS=$PROPERTIES["password@javax.sql.BaseDataSource"] 
+bin/stop.sh -force || true
 sleep 30
 
 #Only run this between 0 and 4AM
@@ -30,22 +32,18 @@ fi
 
 if (( ${cleardb} == 1 )); then
     echo "Clearing database and assets"
+    bin/clean-db.sh
     #Remove Assets
 	rm -rf /var/sakai-assets/trunk-mysql/*
-    rm -rf sakai/elasticsearch/*
-
-	mysql -h ${MYSQLDB}  -u ${DBNAME} -p${MYSQLQATRUNKDB} -e "drop database ${DBNAME}"
-	sleep 10
-	mysql -h ${MYSQLDB}  -u ${DBNAME} -p${MYSQLQATRUNKDB} -e "create database ${DBNAME} character set utf8"    
 fi
 
-rm -rf work/Catalina logs/* webapps components shared/lib/ lib temp/* sakai/archive-unzip/*
+bin/clean-code.sh
 tar xzf /tmp/trunkbuild.tar.gz
-nohup bin/startup.sh
+nohup bin/start.sh
 
 if (( ${cleardb} == 1 )); then
     if [ -f "${DBSCRIPT}" ]; then
         sleep 15m 
-        mysql -f -h ${MYSQLDB} -u ${DBNAME} -p${MYSQLQATRUNKDB} ${DBNAME} -e "source ${DBSCRIPT}"
+        mysql -f -h ${DBHOST} -u ${DBUSER} -p${DBPASS} ${DBNAME} -e "source ${DBSCRIPT}"
     fi
 fi
