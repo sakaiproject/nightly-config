30407f5637836a4b5143f9637b22ba1591d292c9
Earle Nietzel <earle@longsight.com>
2022-11-21 10:53:34 -0500

    Add nightly restarts for trunk-maria and trunk-mysql8

---
+++ restarts/mysql8-trunk.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/zsh
+
+# Jenkins variables
+#  CLEAR_DB - (1/0) 
+#  BUILD_ID is neeeded so Jenkins ProcessTreeKiller doesn't kill off sakai
+BUILD_ID="bin/start.sh"
+
+CATALINA_BASE=/var/trunk-mysql8
+cp master.properties ${CATALINA_BASE}/sakai/sakai.properties
+cd ${CATALINA_BASE}
+source bin/common.sh
+
+DBSCRIPT="${WORKSPACE}/master-mysql.sql"
+DBHOST=$(echo $PROPERTIES["url@javax.sql.BaseDataSource"] | $GREP_CMD -oP '(?<=:\/\/).+(?=:)')
+DBNAME=$(echo $PROPERTIES["url@javax.sql.BaseDataSource"] | $GREP_CMD -oP '(?<=\/)\w+(?=\?)')
+DBUSER=$PROPERTIES["username@javax.sql.BaseDataSource"]
+DBPASS=$PROPERTIES["password@javax.sql.BaseDataSource"] 
+bin/stop.sh -force || true
+sleep 30
+
+#Only run this between 0 and 5AM
+typeset -i chour=10#`/bin/date +"%H"`
+typeset -i dow=10#`/bin/date +"%u"`
+# This is %u so 1 (Monday) - 7 (Sunday)
+typeset -i dowclear=1
+typeset -i shour=0
+typeset -i ehour=5
+typeset -i cleardb=${CLEAR_DB:-0}
+
+echo "Current hour is ${chour}. Current day is ${dow}"
+
+if (( (${chour} >= ${shour}) && (${chour} <= ${ehour}) )); then
+    echo "Dow is ${dow} clear on ${dowclear}"
+    if (( ${dow} == ${dowclear} )); then
+        cleardb=1
+    fi
+fi
+
+if (( ${cleardb} == 1 )); then
+    echo "Clearing database and assets"
+    bin/clean-db.sh
+    #Remove Assets
+fi
+
+bin/clean-code.sh
+tar xzf /tmp/trunkbuild.tar.gz
+bin/start.sh
+
+if (( ${cleardb} == 1 )); then
+    if [ -f "${DBSCRIPT}" ]; then
+    	sleep 15m 
+        mysql -f -h ${DBHOST} -u ${DBUSER} -p${DBPASS} ${DBNAME} -e "source ${DBSCRIPT}"
+    fi
+fi
