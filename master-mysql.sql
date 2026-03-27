ff05bf3e2b3877636189db9cffaa1c0f3063e921
Earle Nietzel <earle.nietzel@getstellarcode.com>
2026-03-27 13:45:15 -0400

    update values after removing lti13_settings

--- master-mysql.sql
+++ master-mysql.sql
@@ -18,11 +18,11 @@ DROP TABLE UPDATE_SAM_AUTO_IDS;
 -- Add automatic link to WarpWire nightly
 INSERT INTO `lti_tools` (`SITE_ID`, `title`, `fa_icon`, `description`, `status`, `visible`, `deployment_id`, `launch`, `consumerkey`, `secret`, `frameheight`, `siteinfoconfig`, `sendname`, `sendemailaddr`, `allowoutcomes`, `allowroster`, `pl_launch`, `pl_linkselection`, `pl_contenteditor`, `pl_importitem`, `pl_fileitem`, `pl_assessmentselection`, `newpage`, `debug`, `custom`, `lti13`, `xmlimport`, `splash`, `created_at`, `updated_at`, `pl_lessonsselection`) 
 VALUES
-(NULL, 'WarpWire Test Server', NULL, NULL, 0, 0,  NULL, 'https://sakainightly.warpwire.net/api/ltix/?mode=plugin', 'lti:sakainightly:01', 'gsAM:wUeZRn2-kJERm-OkqrKB-nJxdke-webMOj-pj8vL2', NULL, 0, 1, 1, 1, 1, NULL, 1, 1, NULL, NULL, NULL, 0, 0, NULL, 0, NULL, NULL, NULL, NOW(), NOW(), 1);
+(NULL, 'WarpWire Test Server', NULL, NULL, 0, 0,  NULL, 'https://sakainightly.warpwire.net/api/ltix/?mode=plugin', 'lti:sakainightly:01', 'gsAM:wUeZRn2-kJERm-OkqrKB-nJxdke-webMOj-pj8vL2', NULL, 0, 1, 1, 1, 1, NULL, 1, 1, NULL, NULL, NULL, 0, 0, NULL, 0, NULL, NULL, NOW(), NOW(), 1);
  
 INSERT INTO `lti_tools` (`SITE_ID`, `title`, `fa_icon`, `description`, `status`, `visible`, `deployment_id`, `launch`,  `consumerkey`,  `secret`,  `frameheight`, `siteinfoconfig`, `sendname`, `sendemailaddr`, `allowoutcomes`, `allowroster`, `pl_launch`, `pl_linkselection`, `pl_contenteditor`, `pl_importitem`, `pl_fileitem`, `pl_assessmentselection`, `newpage`, `debug`, `custom`, `lti13`, `xmlimport`, `splash`, `created_at`, `updated_at`, `pl_lessonsselection`) 
 VALUES
-(NULL, 'TsugiCloud App Store', NULL, NULL, 0, 0,  NULL, 'https://test.tsugicloud.org/tsugi/lti/store/', '12345', 'secret', NULL, 0, 1, 1, 1, 1, NULL, 1, 1, NULL, NULL, 1, 0, 0, NULL, 0, NULL, NULL, NULL, NOW(), NOW(), 1);
+(NULL, 'TsugiCloud App Store', NULL, NULL, 0, 0,  NULL, 'https://test.tsugicloud.org/tsugi/lti/store/', '12345', 'secret', NULL, 0, 1, 1, 1, 1, NULL, 1, 1, NULL, NULL, 1, 0, 0, NULL, 0, NULL, NULL, NOW(), NOW(), 1);
 
 INSERT INTO `lti_tools` (`SITE_ID`, `title`, `fa_icon`, `description`, `status`, `visible`,
 `deployment_id`, `launch`,  `consumerkey`,  `secret`,  
@@ -35,5 +35,5 @@ VALUES
 NULL, 'https://sakai.tsugicloud.org/mod/trophy/', '12345', 'secret', 
 1200, 0, 1, 1, 1, 1, 1,
 1, 0, 1, 1, 0, 0, 1, 1,
-2, 0, NULL, 0, NULL, NULL, NULL,
+2, 0, NULL, 0, NULL, NULL,
 NOW(), NOW());
